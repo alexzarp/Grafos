a2343e7c44b0778b4ddb3d822df2b11881b54658
Alex Sandro <alexszds@gmail.com>
2021-04-07 19:23:52 -0300

    Pronto e ruim

--- BFS+DFS.c
+++ BFS+DFS.c
@@ -3,6 +3,8 @@
 #include <limits.h>
 #include <stdlib.h>
 
+#define TAM 7
+
 struct Controle {
     int valor;
     struct Controle *prox;
@@ -11,7 +13,7 @@ typedef struct Controle controle;
 
 void printControle (controle *raiz) {
     controle *p = raiz->prox;
-    printf("Estado da fila:\n|");
+    //printf("Estado da fila:\n|");
     while (p != NULL) {
         printf(" %d |", p->valor);
         p = p->prox;    
@@ -52,32 +54,71 @@ int lePrimeiro (controle *raiz){
     return value;
 }
 
+void printTabelaBFS(int tabelaBFS[TAM][4], int tamanho, int i) {
+    printf("\nEstado atual\n");
+    printf("vert\t|dist\t|ant\t|visit\n");
+    for (int linha = 0; linha < tamanho; linha++){
+        for (int coluna = 0; coluna < 4; coluna++){
+    // Vertice 0| Distancia 1| Anterior 2| Visitado(cor)3
+            if (tabelaBFS[linha][coluna] == INT_MAX || tabelaBFS[linha][coluna] == INT_MIN)
+                printf("∞\t|");
+            if ((tabelaBFS[linha][coluna] == 0) && (coluna == 3))
+                printf("bran\t|");
+            if ((tabelaBFS[linha][coluna] == 1) && (coluna == 3))
+                printf("cinz\t|");
+            if ((tabelaBFS[linha][coluna] > 1) && (coluna == 3))
+                printf("pret\t|");
+            else
+                printf("%d\t|", tabelaBFS[linha][coluna]);
+            
+        }
+        if ((linha == i) && (i > -1) && (i < tamanho))
+            printf("← Visitando este vertice");
+        printf("\n");
+    }
+}
+
+void printTabelaDFS(int tabelaDFS[TAM][5], int tamanho, int i) {
+    printf("\nEstado atual\n");
+    printf("vert\t|dist\t|ant\t|visit\n");
+    for (int linha = 0; linha < tamanho; linha++){
+        //printf("Linha %d", linha);
+        for (int coluna = 0; coluna < 5; coluna++){
+            if (tabelaDFS[linha][coluna])
+                printf("%d\t|", tabelaDFS[linha][coluna]);
+            if (tabelaDFS[linha][coluna])
+                printf("%d|\t", tabelaDFS[linha][coluna]);
+            if (tabelaDFS[linha][coluna])
+                printf("%d|\t", tabelaDFS[linha][coluna]);
+            if (tabelaDFS[linha][coluna])
+                printf("%d|\t", tabelaDFS[linha][coluna]);
+            if (tabelaDFS[linha][coluna]){
+                if (tabelaDFS[linha][coluna] == 0)
+                    printf("bran|\t");
+                else if (tabelaDFS[linha][coluna] == 1)
+                    printf("cinz|\t");
+                else
+                    printf("pret|\t");
+            }
+           // printf(" Coluna %d", coluna);
+        }
+        if ((linha == i) && (i > -1) && (i < tamanho))
+            printf("← Visitando este vertice");
+        printf("\n");
+    }
+}
+
 // Vertice 0| Distancia 1| Anterior 2| Visitado(cor)3
-void bfs (int mat[7][7], int tabelaBFS[7][4], controle *fila, int tamanho, int origem, int destino) {
+void bfs (int mat[TAM][TAM], int tabelaBFS[TAM][4], controle *fila, int tamanho, int origem, int destino) {
     insereFim(fila, origem);
     int i = origem;
     tabelaBFS[i][1] = 0; //→ distancia 0
-    int contagemPassos = 0;
     while (i < tamanho){
         tabelaBFS[i][3]++; //→ gray
         printf("\nVisitando vértice %d", i);
         for(int j = 0; j < tamanho; j++){
             if ((mat[i][j] != 0) && (tabelaBFS[j][3] < 1)){
                 tabelaBFS[j][1] = 1;
-                // for (int linha = i; linha < tamanho; linha++){
-                //     for (int coluna = j; coluna < 4; coluna++) {
-                //         if (tabelaBFS[linha][1] == INT_MAX) {
-                //             tabelaBFS[linha][1] = 1;
-                //         } else {
-                //             if (tabelaBFS[linha][2] == origem) 
-                //                 break;
-                //             else {
-                //                 tabelaBFS[linha][1]++;
-                //                 linha = tabelaBFS[linha][2];
-                //             } 
-                //         }
-                //     }
-                // }
                 tabelaBFS[j][2] = i;
                 insereFim(fila, j);
                 tabelaBFS[j][3]++;
@@ -87,29 +128,10 @@ void bfs (int mat[7][7], int tabelaBFS[7][4], controle *fila, int tamanho, int o
             }
         }
         tabelaBFS[i][3]++;
-//--------------Print da tabela--------------------------------------------
-        printf("\nEstado atual\n");
-        printf("vert\t|dist\t|ant\t|visit\n");
-        for (int linha = 0; linha < tamanho; linha++){
-            for (int coluna = 0; coluna < 4; coluna++){
-        // Vertice 0| Distancia 1| Anterior 2| Visitado(cor)3
-        
-                if (tabelaBFS[linha][coluna] == INT_MAX || tabelaBFS[linha][coluna] == INT_MIN)
-                    printf("∞\t|");
-                if ((tabelaBFS[linha][coluna] == 0) && (coluna == 3))
-                    printf("bran\t|");
-                if ((tabelaBFS[linha][coluna] == 1) && (coluna == 3))
-                    printf("cinz\t|");
-                if ((tabelaBFS[linha][coluna] > 1) && (coluna == 3))
-                    printf("pret\t|");
-                else
-                    printf("%d\t|", tabelaBFS[linha][coluna]);
-            }
-            if ((linha == i) && (i > -1) && (i < tamanho))
-                printf("← Visitando este vertice");
-            printf("\n");
-        }
-//-------------------------------------------------------------------------
+
+        printTabelaBFS (tabelaBFS, tamanho, i);
+
+        printf("Estado da fila:\n|");
         printControle(fila);
 
         removePrimeiro(fila);
@@ -128,33 +150,100 @@ void bfs (int mat[7][7], int tabelaBFS[7][4], controle *fila, int tamanho, int o
     
 }
 
-//                                (0)(1)(2)(3)(4)(5)(6)
-int main() {//                     A  B  C  D  E  F  G
-    int matAdja[7][7] = {/*(0) A*/{0, 7, 0, 5, 0, 0, 0},
-            /*  ↑  ↑       (1) B*/{7, 0, 8, 9, 7, 0, 0},
-            /*  L  C       (2) C*/{0, 8, 0, 0, 5, 0, 0},
-                         /*(3) D*/{5, 9, 0, 0,15, 6, 0},
-                         /*(4) E*/{0, 7, 5,15, 0, 8, 9},         //00  01  02  03
-                         /*(5) F*/{0, 0, 0, 6, 8, 0,11},         //10  11  12  13
-                         /*(6) G*/{0, 0, 0, 0, 9,11, 0}          //20  21  22  23
-    };                                                           //30  31  32  33
-             // Vertice    Distância   Ligado com   Visitado
-    int tabelaBFS[7][4] = {{0, INT_MAX, -1, 0},
-                           {1, INT_MAX, -1, 0},
-                           {2, INT_MAX, -1, 0},
-                           {3, INT_MAX, -1, 0},
-                           {4, INT_MAX, -1, 0},
-                           {5, INT_MAX, -1, 0},
-                           {6, INT_MAX, -1, 0},
-    };                                   // ↑ 0 = white, 1 = gray, 2 = black
-    //essa é minha pilha
+void dfs (int mat[TAM][TAM], int tabelaDFS[TAM][5], controle *pilha, int tamanho, int origem, int destino) {
+    insereInicio(pilha, origem);
+    int i = origem;
+    int passos = 1;
+    tabelaDFS[i][1] = passos;//→ encontro no momento 1
+    while (i < tamanho){
+        tabelaDFS[i][4]++; //→ gray
+        printf("\nVisitando vértice %d", i);
+        for(int j = 0; j < tamanho; j++){
+            if ((mat[i][j] != 0) && (tabelaDFS[j][4] < 1)){
+                passos++;
+                tabelaDFS[j][1] = passos;
+                tabelaDFS[j][3] = i;
+                tabelaDFS[j][4]++;
+                insereInicio(pilha, j);
+            }
+            else {
+                passos++;
+                tabelaDFS[j][2] = passos;
+            }
+        }
+        tabelaDFS[i][4]++;
+        printf ("teste");
+        printTabelaDFS (tabelaDFS, tamanho, i);
+
+        printf("Estado da pilha:\n|");
+        printControle(pilha);
+
+        removePrimeiro(pilha);
+        
+        if (destino == i){
+            printf("Vertice encontrado, interrompendo navegação\n");
+            return;
+        }
+        else if (pilha->prox == NULL){
+            printf("Não foi possível ligar-se ao vertice\n");
+            return;
+        }
+
+        i = lePrimeiro(pilha);
+    }
+}
+
+//                                    (0)(1)(2)(3)(4)(5)(6)
+int main() {//                         A  B  C  D  E  F  G
+    int matAdja[TAM][TAM] = {/*(0) A*/{0, 7, 0, 5, 0, 0, 0},
+            /*   ↑    ↑        (1) B*/{7, 0, 8, 9, 7, 0, 0},
+            /*   L    C        (2) C*/{0, 8, 0, 0, 5, 0, 0},
+                             /*(3) D*/{5, 9, 0, 0,15, 6, 0},
+                             /*(4) E*/{0, 7, 5,15, 0, 8, 9},         //00  01  02  03
+                             /*(5) F*/{0, 0, 0, 6, 8, 0,11},         //10  11  12  13
+                             /*(6) G*/{0, 0, 0, 0, 9,11, 0}          //20  21  22  23
+    };                                                               //30  31  32  33
+             // Vertice    Distância   Anterior   Visitado
+    int tabelaBFS[TAM][4] = {{0, INT_MAX, -1, 0},
+                             {1, INT_MAX, -1, 0},
+                             {2, INT_MAX, -1, 0},
+                             {3, INT_MAX, -1, 0},
+                             {4, INT_MAX, -1, 0},
+                             {5, INT_MAX, -1, 0},
+                             {6, INT_MAX, -1, 0},
+    };                                     // ↑ 0 = white, 1 = gray, 2 = black
+
+    // Vertice  Momento_encotro  Momento_busca_completa  Anterior  Visitado
+    int tabelaDFS[TAM][5] = {{0, 0, 0, -1, 0},
+                             {1, 0, 0, -1, 0},
+                             {2, 0, 0, -1, 0},
+                             {3, 0, 0, -1, 0},
+                             {4, 0, 0, -1, 0},
+                             {5, 0, 0, -1, 0},
+                             {6, 0, 0, -1, 0},
+    };                                  // ↑ 0 = white, 1 = gray, 2 = black
+
+    //essa é minha pilha ou fila
     controle *raiz = (controle*) malloc(sizeof(controle));
     raiz->prox = NULL;
-    //raiz->valor = NULL;
-    int origem, destino;
-    printf("Qual origem destino: ");
+
+    int origem, destino ;
+    printf("Origem destino: ");
     scanf("%d%d", &origem, &destino);
-    bfs(matAdja, tabelaBFS, raiz, 7, origem, destino);
+    int expression;
+    printf("\nEscolha DFS - 1 ou BFS - 2: ");
+    scanf("%d", &expression);
+    switch (expression) {
+    case 1:
+        dfs(matAdja, tabelaDFS, raiz, TAM, origem, destino);
+        break;
     
+    default:
+        bfs(matAdja, tabelaBFS, raiz, TAM, origem, destino);
+        break;
+    }
+
+    // printTabelaDFS(tabelaDFS, TAM, 0);
+    // printTabelaBFS(tabelaBFS, TAM, 0);
     return 0;
 }
